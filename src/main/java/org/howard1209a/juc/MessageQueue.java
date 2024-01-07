4d1a1ac8f2bae124773d8c710c735d5f1ee8e607
howard1209a <howard1209a@163.com>
2024-01-07 21:16:00 +0800

    update

---
+++ src/main/java/org/howard1209a/juc/MessageQueue.java
@@ -0,0 +1,63 @@
+package org.howard1209a.juc;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 固定容量阻塞式消息队列
+ */
+public class MessageQueue {
+    private List<Message> messages;
+    private int capacity;
+
+    public MessageQueue(int capacity) {
+        this.messages = new ArrayList<>();
+        this.capacity = capacity;
+    }
+
+    /**
+     * 阻塞式进队
+     */
+    public synchronized void add(Message message) {
+        while (messages.size() == capacity) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        notifyAll();
+        messages.add(message);
+    }
+
+    /**
+     * 阻塞式出队
+     */
+    public Message poll() {
+        synchronized (this) {
+            while (messages.size() == 0) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            notifyAll();
+            return messages.remove(0);
+        }
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    private static class Message {
+        private int id;
+        private Object value;
+    }
+}
